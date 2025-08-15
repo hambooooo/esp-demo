7613aad6fadd49c0a31662bb93a84152c4edf31d
董海明 <donghaiming@donghaiming.com>
2025-08-15 11:20:51 +0800

    Drive ST7789 display

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,17 +6,34 @@
     holding buffers for the duration of a data transfer."
 )]
 
-use bt_hci::controller::ExternalController;
+extern crate alloc;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
+
 use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
+use esp_hal::delay::Delay;
+use esp_hal::gpio::{Level, Output, OutputConfig};
+use esp_hal::rtc_cntl::Rtc;
+use esp_hal::spi::master::{Config, Spi};
 use esp_hal::timer::systimer::SystemTimer;
 use esp_hal::timer::timg::TimerGroup;
-use esp_wifi::ble::controller::BleConnector;
 use log::info;
 
-extern crate alloc;
+use embedded_hal_bus::spi::ExclusiveDevice;
+
+use embedded_graphics::{
+    pixelcolor::Rgb565,
+    prelude::*,
+    primitives::{Circle, Primitive, PrimitiveStyle, Triangle},
+};
+
+// Provides the parallel port and display interface builders
+use mipidsi::interface::SpiInterface;
+
+use mipidsi::options::ColorOrder;
+// Provides the Display builder
+use mipidsi::{models::ST7789, Builder};
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
@@ -33,21 +50,61 @@ async fn main(spawner: Spawner) {
 
     esp_alloc::heap_allocator!(size: 64 * 1024);
 
+    // Disable the RTC and TIMG watchdog timers
+    let mut rtc = Rtc::new(peripherals.LPWR);
+    let timer_group0 = TimerGroup::new(peripherals.TIMG0);
+    let mut wdt0 = timer_group0.wdt;
+    let timer_group1 = TimerGroup::new(peripherals.TIMG1);
+    let mut wdt1 = timer_group1.wdt;
+    rtc.swd.disable();
+    rtc.rwdt.disable();
+    wdt0.disable();
+    wdt1.disable();
+
+    // Init embassy
     let timer0 = SystemTimer::new(peripherals.SYSTIMER);
     esp_hal_embassy::init(timer0.alarm0);
 
     info!("Embassy initialized!");
 
-    let rng = esp_hal::rng::Rng::new(peripherals.RNG);
-    let timer1 = TimerGroup::new(peripherals.TIMG0);
-    let wifi_init =
-        esp_wifi::init(timer1.timer0, rng).expect("Failed to initialize WIFI/BLE controller");
-    // find more examples https://github.com/embassy-rs/trouble/tree/main/examples/esp32
-    let transport = BleConnector::new(&wifi_init, peripherals.BT);
-    let _ble_controller = ExternalController::<_, 20>::new(transport);
-
     // TODO: Spawn some tasks
     let _ = spawner;
+
+    // Define the delay struct, needed for the display driver
+    let mut delay = Delay::new();
+
+    // Define the Data/Command select pin as a digital output
+    let dc = Output::new(peripherals.GPIO7, Level::Low, OutputConfig::default());
+    // Define the reset pin as digital outputs and make it high
+    let mut rst = Output::new(peripherals.GPIO8, Level::Low, OutputConfig::default());
+    rst.set_high();
+
+    // Define the SPI pins and create the SPI interface
+    let spi = Spi::new(peripherals.SPI2, Config::default())
+        .unwrap()
+        .with_sck(peripherals.GPIO0)
+        .with_mosi(peripherals.GPIO1)
+        .with_miso(peripherals.GPIO2);
+
+    let cs_output = Output::new(peripherals.GPIO3, Level::High, OutputConfig::default());
+    let spi_device = ExclusiveDevice::new_no_delay(spi, cs_output).unwrap();
+
+    let mut buffer = [0_u8; 512];
+
+    // Define the display interface with no chip select
+    let di = SpiInterface::new(spi_device, dc, &mut buffer);
+
+    // Define the display from the display interface and initialize it
+    let mut display = Builder::new(ST7789, di)
+        .reset_pin(rst)
+        .init(&mut delay)
+        .unwrap();
+
+    // Make the display all black
+    display.clear(Rgb565::BLACK).unwrap();
+
+    // Draw a smiley face with white eyes and a red mouth
+    draw_smiley(&mut display).unwrap();
 
     loop {
         info!("Hello world!");
@@ -55,4 +112,36 @@ async fn main(spawner: Spawner) {
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-rc.0/examples/src/bin
+}
+
+fn draw_smiley<T: DrawTarget<Color = Rgb565>>(display: &mut T) -> Result<(), T::Error> {
+    // Draw the left eye as a circle located at (50, 100), with a diameter of 40, filled with white
+    Circle::new(Point::new(50, 100), 40)
+        .into_styled(PrimitiveStyle::with_fill(Rgb565::WHITE))
+        .draw(display)?;
+
+    // Draw the right eye as a circle located at (50, 200), with a diameter of 40, filled with white
+    Circle::new(Point::new(50, 200), 40)
+        .into_styled(PrimitiveStyle::with_fill(Rgb565::WHITE))
+        .draw(display)?;
+
+    // Draw an upside down red triangle to represent a smiling mouth
+    Triangle::new(
+        Point::new(130, 140),
+        Point::new(130, 200),
+        Point::new(160, 170),
+    )
+    .into_styled(PrimitiveStyle::with_fill(Rgb565::RED))
+    .draw(display)?;
+
+    // Cover the top part of the mouth with a black triangle so it looks closed instead of open
+    Triangle::new(
+        Point::new(130, 150),
+        Point::new(130, 190),
+        Point::new(150, 170),
+    )
+    .into_styled(PrimitiveStyle::with_fill(Rgb565::BLACK))
+    .draw(display)?;
+
+    Ok(())
 }
